bd6ddb0b252fcdb411e3b76b72e9ee262ac157b4
Maha Malik <malikms@uci.edu>
2020-04-04 16:38:55 -0700

    Initial Commit

---
+++ tippy/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  tippy
+//
+//  Created by Maha Malik on 4/4/20.
+//  Copyright © 2020 Maha Malik. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

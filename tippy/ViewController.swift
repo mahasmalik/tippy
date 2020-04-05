ad0496fe11bc5f27f12fac4c98c3b9047822a166
Maha Malik <malikms@uci.edu>
2020-04-04 19:14:26 -0700

    Added tip calculator features

--- tippy/ViewController.swift
+++ tippy/ViewController.swift
@@ -10,11 +10,39 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var billField: UITextField!
+    @IBOutlet weak var tipLabel: UILabel!
+    @IBOutlet weak var totalLabel: UILabel!
+    @IBOutlet weak var tipControl: UISegmentedControl!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func onTap(_ sender: Any) {
+        view.endEditing(true)
+    }
+    
+    @IBAction func calculateTip(_ sender: Any) {
+        
+        // Get the bill amount
+        let bill = Double(billField.text!) ?? 0 // invalid input is 0
+        let tipPercentages = [0.15, 0.18, 0.2]
+        
+        // Calculate the tip and total
+        let tip = bill * tipPercentages[tipControl.selectedSegmentIndex]
+        let total = bill + tip
+        
+        // Update the tip and total label
+        tipLabel.text = String(format: "$%.2f", tip)
+        totalLabel.text = String(format: "$%.2f", total)
+    }
+    @IBAction func resetTap(_ sender: Any) {
+        
+        billField.text = ""
+        tipLabel.text = "$0.00"
+        totalLabel.text = "$0.00"
+    }
 }
 
